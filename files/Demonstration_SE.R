d4f286fdaf728c901ab80a6d7af72a951111fbb8
Mauricio Bucca <mb2322@cornell.edu>
2019-09-20 16:21:30 +0200

    Add files via upload

---
+++ files/Demonstration_SE.R
@@ -0,0 +1,44 @@
+
+set.seed(13232)
+
+# Teoretical parameters
+mu
+sigma = (105/36)^(1/2)
+n = 500
+
+
+
+# A random sample of dice throws 
+
+our_sample <- sample(x = 1:6, size = n, replace = TRUE)
+plot(table(our_sample), xlab = 'Values', ylab = 'Frequency', main = '500 Rolls of 1 Fair Dice')
+mean(our_sample)
+
+
+#  Demonstration of the distrution of mu_hat
+
+mues_hat = NULL
+
+for (i in 1:1000) {
+  
+  a_sample <- sample(x = 1:6, size = 500, replace = TRUE)
+  mu_hat <- mean(a_sample) 
+  mues_hat <- c(mues_hat,mu_hat)
+  
+}
+
+
+# Mean and SD of mu_hat 
+
+mean_mu_hat <- mean(mues_hat)
+sd_mu_hat  <- sd(mues_hat)
+
+# mean of mu_hat  = mu
+mean_mu_hat; mu
+
+# sd of mu_hat  = sigma/sqrt(n)
+sd_mu_hat; sigma/sqrt(n)
+
+# Distribution of mu_hat: approximately normal
+hist(mues_hat)
+abline(v = mean(mues_hat), col="red", lwd=3, lty=2)
